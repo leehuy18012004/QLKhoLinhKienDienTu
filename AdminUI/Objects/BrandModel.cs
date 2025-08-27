ea7f83ad78b6db43a7b22edfc5ce18f54ef8e627
LeeHuy18 <levonhathuy12b5@gmail.com>
2025-08-27 20:42:04 +0700

    First commit

---
+++ AdminUI/Objects/BrandModel.cs
@@ -0,0 +1,34 @@
+﻿namespace AdminUI.Objects
+{
+    public class BrandModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public BrandModel(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public BrandModel()
+        { }
+        public BrandModel(BrandModel model)
+        {
+            Id = model.Id;
+            Name = model.Name;
+        }
+    }
+    public class BrandResponse
+    {
+        public string Message { get; set; }
+        public BrandModel Data { get; set; }
+
+    }
+    public class ListBrandModel
+    {
+        public string Message { get; set; }
+        public List<BrandModel> Data { get; set; }
+
+    }
+}
+
---
+++ AdminUI/Objects/CategoryModel.cs
@@ -0,0 +1,36 @@
+﻿using Bogus.DataSets;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace AdminUI.Objects
+{
+    public class CategoryModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public CategoryModel(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public CategoryModel()
+        { }
+        public CategoryModel(CategoryModel model)
+        {
+            Id = model.Id;
+            Name = model.Name;
+        }
+    }
+    public class CategoryResponse
+    {
+        public string Message { get; set; }
+        public CategoryModel Data { get; set; }
+
+    }
+    public class ListCategoryModel
+    {
+        public string Message { get; set; }
+        public List<CategoryModel> Data { get; set; }
+
+    }
+}
